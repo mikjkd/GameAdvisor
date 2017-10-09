1f60a3ec465ce021dbeebca972fd34a45395c69f
Fabio <fabio.dispaizo@studenti.unina2.it>
2017-10-09 14:05:23 +0200

    Aggiunta metodi per scaricare icone generi, modifica alla classe DataGenere e revisioni grafiche.

--- app/src/main/java/com/miche/gameadvisorprova3/Model/DataGenere.java
+++ app/src/main/java/com/miche/gameadvisorprova3/Model/DataGenere.java
@@ -1,18 +1,25 @@
 package com.miche.gameadvisorprova3.Model;
 
+import java.io.Serializable;
+
 /**
  * Created by miche on 07/10/2017.
  */
 
-public class DataGenere {
+public class DataGenere implements Serializable {
     private String KeyGenere;
     private String Titolo;
     private String KeyGioco;
+
+    private String URLico;
+    private String Pathlocale;
+
     public DataGenere(){}
-    public DataGenere(String genere, String titolo, String key) {
+    public DataGenere(String genere, String titolo, String key, String URLico) {
         KeyGenere = genere;
         Titolo = titolo;
         KeyGioco = key;
+        this.URLico = URLico;
     }
 
     public String getKeyGenere() { return KeyGenere; }
@@ -26,4 +33,20 @@ public class DataGenere {
     public String getKeyGioco() { return KeyGioco; }
 
     public void setKeyGioco(String key) { KeyGioco = key; }
+
+    public String getURLico() {
+        return URLico;
+    }
+
+    public void setURLico(String URLico) {
+        this.URLico = URLico;
+    }
+
+    public String getPathlocale() {
+        return Pathlocale;
+    }
+
+    public void setPathlocale(String pathlocale) {
+        Pathlocale = pathlocale;
+    }
 }
--- app/src/main/java/com/miche/gameadvisorprova3/Model/DatabaseLink.java
+++ app/src/main/java/com/miche/gameadvisorprova3/Model/DatabaseLink.java
@@ -53,6 +53,10 @@ public class DatabaseLink {
     private ValueEventListener listenerGiochi;
     private ValueEventListener listenerGenere;
     private Bitmap immagine;
+
+    private Bitmap icona;
+
+
     public DatabaseLink(){
         giochi= new ArrayList<>();
         generi = new ArrayList<>();
@@ -127,6 +131,12 @@ public class DatabaseLink {
                     generi.add(gen);
 
                 }
+                scaricaIcona(new DatabaseLink.BitmapListener(){
+                    @Override
+                    public void BitmapPronta() {
+                        notificaGenere.generiAggiornati();
+                    }
+                });
                 notificaGenere.generiAggiornati();
             }
 
@@ -172,11 +182,44 @@ public class DatabaseLink {
         }
     }
 
+    public void scaricaIcona(final BitmapListener immagineCaricata ){
+
+        final Bitmap[] bmp = new Bitmap[1];
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+
+        for (final DataGenere dge : generi){
+
+            final File localFile;
+
+            try{
+                localFile = File.createTempFile(dge.getKeyGenere(),".jpg");
+                storageRef.child("Iconegeneri/"+dge.getKeyGenere()+".jpg").getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        dge.setPathlocale(localFile.getAbsolutePath());
+                        Log.e("path: ",localFile.getAbsolutePath());
+
+                        immagineCaricata.BitmapPronta();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("Miss","Fallito");
+                    }
+                });
+            } catch (IOException e) {
+                Log.e("Errore","Try Catch");
+            }
+
+        }
+
+    }
+
     public List<DataGioco> elencoGiochi(){
         return giochi;
     }
     public List<DataGenere> elencoGenere() { return generi; }
-    public void setImmagineBitmap(Bitmap img){this.immagine= img;}
-    public Bitmap getImmagineBitmap(){ return this.immagine; }
 
 }

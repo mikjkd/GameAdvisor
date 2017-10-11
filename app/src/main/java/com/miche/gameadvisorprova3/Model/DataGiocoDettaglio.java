e7fa02ee8a5bda00cb61ce33e5a8fc5d6ef452cd
mikjkd <micheledigio.mdg@gmail.com>
2017-10-11 12:38:49 +0200

    Storage e aggiornamenti

---
+++ app/src/main/java/com/miche/gameadvisorprova3/Model/DataGiocoDettaglio.java
@@ -0,0 +1,90 @@
+package com.miche.gameadvisorprova3.Model;
+
+import java.io.Serializable;
+
+/**
+ * Created by miche on 11/10/2017.
+ */
+
+public class DataGiocoDettaglio implements Serializable {
+    private String Genere;
+    private String Link;
+    private String Titolo;
+    private String Descrizione;
+    private String URLimg;
+    private String Key;
+    private String UrlIconaLocale;
+    private String UrlImmagineLocale;
+    // Bitmap immagine;
+
+
+    public DataGiocoDettaglio(){}
+
+    public String getKey() {
+        return Key;
+    }
+
+    public void setKey(String key) {
+        Key = key;
+    }
+
+    public DataGiocoDettaglio(String genere, String link, String titolo, String descrizione, String URLimg) {
+        Genere = genere;
+        Link = link;
+        Titolo = titolo;
+        Descrizione = descrizione;
+        this.URLimg = URLimg;
+    }
+
+    public String getGenere() {
+        return Genere;
+    }
+
+    public void setGenere(String genere) {
+        Genere = genere;
+    }
+
+    public String getLink() {
+        return Link;
+    }
+
+    public void setLink(String link) {
+        Link = link;
+    }
+
+    public String getTitolo() {
+        return Titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        Titolo = titolo;
+    }
+
+    public String getDescrizione() {
+        return Descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        Descrizione = descrizione;
+    }
+
+    public String getURLimg() {
+        return URLimg;
+    }
+
+    public void setURLimg(String URLimg) {
+        this.URLimg = URLimg;
+    }
+
+    public String getUrlIconaLocale() {
+        return UrlIconaLocale;
+    }
+
+    public void setUrlIconaLocale(String urlIconaLocale) {
+        UrlIconaLocale = urlIconaLocale;
+    }
+
+    public String getUrlImmagineLocale() {   return UrlImmagineLocale; }
+
+    public void setUrlImmagineLocale(String urlImmagineLocale) { UrlImmagineLocale = urlImmagineLocale; }
+}
